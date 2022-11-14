77e4b226470c7accb62deefcc338e3a196714d84
zkmargo <zkmargo@gmail.com>
2022-11-14 16:00:47 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
-        'name': 'Luiz Otávio'
-        
-    })
+        'name': 'Luiz Otávio',
+     })
+    
+    
 
 
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
